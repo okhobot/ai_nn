6ff54a12cd1b5f1f44f82e640e6090c9077f5f90
okhobot <okhobot03@gmail.com>
2026-04-18 13:32:09 +0300

    config_manager

---
+++ config_manager.py
@@ -0,0 +1,18 @@
+import os
+import json
+class ConfigManager:
+    def __init__(self):
+         self.json_config=None
+    def load_config(self, path_to_config="config/config.json"):
+        with open(path_to_config, 'r', encoding='utf-8') as f: 
+            self.json_config = json.load(f)
+    def set_hf_env(self):
+            if self.json_config["cache_dir"] != "":
+                os.environ['HF_HOME'] = self.json_config["cache_dir"]
+            
+            os.environ['HF_TOKEN'] = self.json_config["hf_token"]
+            os.environ['HF_HUB_OFFLINE'] = str(int(self.json_config["offline"]))
+            os.environ['TRANSFORMERS_OFFLINE'] = str(int(self.json_config["offline"]))
+    
+    def get_json_config(self):
+         return self.json_config
